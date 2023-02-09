70452f5c9f1ecf654ce09f4cff4b3a5fda9bba67
陆冬帛 <LdbRn20101208@126.com>
2023-02-09 13:12:59 +0800

    Projects

--- Python/BMI.py
+++ Python/BMI.py
@@ -2,9 +2,9 @@ w = float(input("请输入您的体重(kg):"))
 h = float(input("请输入您的身高(m):"))
 BMI = w / h ** 2
 print("您的BMI值为:", BMI)
-if 20 <= BMI <= 24:
+if 18.5 <= BMI <= 23.9:
     print("正常")
-elif BMI < 20:
+elif BMI < 18.5:
     print("偏瘦")
 else:
     print("偏胖")
