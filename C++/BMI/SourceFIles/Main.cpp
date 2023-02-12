3df4191dbac1f5cdce2470c92cff111b15e6d783
LDB1208 <123877713+LDB1208@users.noreply.github.com>
2023-02-12 11:23:22 +0800

    Add files via upload

---
+++ C++/BMI/SourceFIles/Main.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <stdlib.h>
+#include <Windows.h>
+using namespace std;
+
+int main() {
+	//提示语
+	cout << "This program is used for calculating BMI" << endl;
+	Sleep(2000);
+
+	//接受输入
+	float w = 0.0f;
+	float h = 0.0f;
+	cout << "Your weight(kg):";
+	cin >> w;
+	cout << "Your height(m):";
+	cin >> h;
+
+	//计算
+	float BMI = w / pow(h, 2); //pow(a, b)运算a的b次方
+	cout << "Your BMI is:" << BMI << endl;
+	if (18.5 <= BMI <= 23.9) {
+		cout << "Healthy" << endl;
+	}
+	else if (BMI < 18.5) {
+		cout << "Too thin" << endl;
+	}
+	else {
+		cout << "Too fat" << endl;
+	}
+
+	//结束语
+	cout << "Done" << endl;
+	system("pause");
+	return 0;
+}
