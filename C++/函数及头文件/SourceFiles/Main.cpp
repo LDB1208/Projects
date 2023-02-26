601b5a980e7b05f8202804d60b29a1985c30dbe0
LDB1208 <123877713+LDB1208@users.noreply.github.com>
2023-02-26 21:30:24 +0800

    Add files via upload

---
+++ C++/函数及头文件/SourceFiles/Main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "HeadFile.h" //引入头文件
+using namespace std; //使用std命名空间
+
+/*
+* 本程序演示函数和头文件
+*/
+
+int main() {
+	cout << Multiply(5, 8) << endl; //输出结果
+	cin.get(); //等待用户输入回车,结束程序
+	return 0;
+}
