b90cd2739bbb7f7fd41a1a029f707829cd40ef84
LDB1208 <123877713+LDB1208@users.noreply.github.com>
2023-02-27 19:10:42 +0800

    Add files via upload

---
+++ C++/语法/函数及头文件/SourceFiles/HeadFile.h
@@ -0,0 +1,2 @@
+#pragma once //只引用一次
+int Multiply(int a, int b); //函数声明
---
+++ C++/语法/条件判断语句/SourceFiles/Main.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+/*
+* 本程序演示条件判断语句
+*/
+
+int main() {
+	int a = 5; //声明变量
+	if (a == 6) {
+		//如果a=6,执行此行代码
+		cout << "a = 6" << endl; //终端输出
+	}
+	else if (a < 4) {
+		//如果a不等于6,且a<4,执行此行代码
+		cout << "a < 4" << endl;
+	}
+	else {
+		//如果a不等于6,且a>=4,执行此行代码
+		cout << a << endl;
+	}
+	cin.get();
+	return 0;
+}
